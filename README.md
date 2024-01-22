a2fad44e0979248c4e2939983864cb06104b4ac4
Abdulrazak Idris <Abdulrazakidrisyahaya@gmail.com>
2024-01-22 08:50:37 +0100

    Refactor Code

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Rock Paper Scissors
 
-# An intransitive game, usually played between two people, in which each player simultaneously between one of three items (Rock, Paper, Scissors) with a defined rules.
+# An intransitive game, usually played between two people, in which each player simultaneously choose between one of three items (Rock, Paper, Scissors) with a defined rules.
 
 # RULES
+
 # Rock beats Scissors.
+
 # Scissors beats Paper.
+
 # Paper beats Rock.
--- script.js
+++ script.js
@@ -1,59 +1,58 @@
 // Computer Choice.
 function getComputerChoice() {
-    let choices = [];
-    choices = ["Rock", "Paper", "Scissors"];
-    const randomNumber = Math.random() * choices.length;
-    const index = Math.floor(randomNumber);
-    const selectedChoice = choices[index].toLowerCase();
-return selectedChoice;
+  let choices = [];
+  choices = ["Rock", "Paper", "Scissors"];
+  const randomNumber = Math.random() * choices.length;
+  const index = Math.floor(randomNumber);
+  const selectedChoice = choices[index].toLowerCase();
+  return selectedChoice;
 }
 
 // Player choice.
 // Compare both choices.
 function playRound(userChoice, getComputerChoice) {
-    let playerSelection = userChoice.toLowerCase();
-    if (playerSelection === "rock" && getComputerChoice === "scissors") {
-        return "You Win";
-    } else if (playerSelection === "scissors" && getComputerChoice ==="paper") {
-        return "You Win";
-    } else if (playerSelection === "paper" && getComputerChoice === "rock") {
-        return "You Win";
-    } else if (playerSelection === getComputerChoice) {
-        return ("Draw");
-    } else {
-        return ("You Loose");
-    }
+  let playerSelection = userChoice.toLowerCase();
+  if (playerSelection === "rock" && getComputerChoice === "scissors") {
+    return "You Win";
+  } else if (playerSelection === "scissors" && getComputerChoice === "paper") {
+    return "You Win";
+  } else if (playerSelection === "paper" && getComputerChoice === "rock") {
+    return "You Win";
+  } else if (playerSelection === getComputerChoice) {
+    return "Draw";
+  } else {
+    return "You Loose";
+  }
 }
 
 // Game function implementation!!
 function game() {
-    const playerChoice = (prompt("What your choice").toLowerCase());
-    const computerSelection = getComputerChoice().toLowerCase();
-    return (playRound(playerChoice, computerSelection)).toLowerCase();
-};
-
+  const playerChoice = prompt("What your choice").toLowerCase();
+  const computerSelection = getComputerChoice().toLowerCase();
+  return playRound(playerChoice, computerSelection).toLowerCase();
+}
 
 // Asking user 5 times.
-   let counter = 5;
-   let user = 0;
-   let computer = 0;
+let counter = 5;
+let user = 0;
+let computer = 0;
 
 // Loop n times, save the winner and print it letter!!
-   for (let i = 0; i < counter; i++) {
-      let result = game();
-       if (result === "you win") {
-         user += 1
-         if (user == 3) {
-             document.getElementById("result").innerHTML = "YOU WIN"
-         }
-       } else if (result === "you loose") {
-         computer += 1;
-         if (computer == 3) {
-            document.getElementById("result").innerHTML = "YOU LOOSE"
-         }
-       }
-   }
+for (let i = 0; i < counter; i++) {
+  let result = game();
+  if (result === "you win") {
+    user += 1;
+    if (user == 3) {
+      document.getElementById("result").innerHTML = "YOU WIN";
+    }
+  } else if (result === "you loose") {
+    computer += 1;
+    if (computer == 3) {
+      document.getElementById("result").innerHTML = "YOU LOOSE";
+    }
+  }
+}
 
-   if (user == computer) {
-    document.getElementById("result").innerHTML = "DRAW"
-   }
+if (user == computer) {
+  document.getElementById("result").innerHTML = "DRAW";
+}
